09a5742e6b7841dc07c2b8ab8bdbb59160920104
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-11 02:09:13 +0300

    Add an add toto item function

--- src/index.js
+++ src/index.js
@@ -1,37 +1,10 @@
 import './style.css';
+import List from './crud.js';
 
-const listItems = [
-  {
-    description: 'Write an email',
-    completed: true,
-    index: 0,
-  },
-  {
-    description: 'Laundry',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Read a book',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Submit assignment',
-    completed: false,
-    index: 3,
-  },
-];
-const list = document.querySelector('ul');
 
-listItems.forEach((item) => {
-  const listItem = document.createElement('li');
-  listItem.innerHTML = `
-  <div class="content">
-  <input type="checkbox" class="to-do" name="todo${item.index}" value="${item.completed}">
-  <label for="todo${item.index}"> ${item.description}</label>
-  </div>
-  <i class="fa fa-ellipsis-v" aria-hidden="true"></i>`;
 
-  list.appendChild(listItem);
-});
+const todo = new List()
+todo.display()
+todo.submit()
+console.log(todo)
+
