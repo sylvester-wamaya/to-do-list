acf567ae047ab2a4c70330bde14624c34ca6dd0e
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-12 21:19:51 +0300

    Add blur and focus event listener on list items

--- src/index.js
+++ src/index.js
@@ -9,17 +9,31 @@ todo.submit()
 todo.change()
 console.log(todo)
 
-document.addEventListener("click", (e)=>{
+document.addEventListener("focus", (e)=>{
   
       if(e.target.classList.contains("list-item")){
         const parent = e.target.parentNode.parentNode
-       parent.style.background = "red"
+       parent.style.background = "#ff0"
+       e.target.style.background = "#ff0"
        
        parent.children[1].style.display = "none"
         parent.children[2].style.display = "block"
         parent.children[0].children[0].style.opacity = 0.5
-       
-}})
+        
+}}, true)
+
+document.addEventListener("blur", (e)=>{
+  
+    if(e.target.classList.contains("list-item")){
+      const parent = e.target.parentNode.parentNode
+     parent.style.background = ""
+     e.target.style.background = ""
+     
+     parent.children[1].style.display = "block"
+      parent.children[2].style.display = "none"
+      parent.children[0].children[0].style.opacity = 0
+      
+}}, true)
 
 document.addEventListener("click", (e)=>{
   
@@ -30,6 +44,16 @@ document.addEventListener("click", (e)=>{
       parent.children[2].style.display = "block"     
      
 }})
+
+document.addEventListener("blur", (e)=>{
+  
+    if(e.target.classList.contains("dots")){
+      const parent = e.target.parentNode
+          
+     parent.children[1].style.display = "block"
+      parent.children[2].style.display = "none"     
+     
+}}, true)
 
 
 
