963a45b2b2ee0b109aeeffe181f7f5ea6522ae5a
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-12 20:38:21 +0300

    Add event listener to the trash and dots icons

--- src/index.js
+++ src/index.js
@@ -2,13 +2,34 @@ import './style.css';
 import List from './crud.js';
 
 
-const listIte = document.querySelector('li');
+
 const todo = new List()
 todo.display()
 todo.submit()
 todo.change()
 console.log(todo)
 
+document.addEventListener("click", (e)=>{
+  
+      if(e.target.classList.contains("list-item")){
+        const parent = e.target.parentNode.parentNode
+       parent.style.background = "red"
+       
+       parent.children[1].style.display = "none"
+        parent.children[2].style.display = "block"
+        parent.children[0].children[0].style.opacity = 0.5
+       
+}})
+
+document.addEventListener("click", (e)=>{
+  
+    if(e.target.classList.contains("dots")){
+      const parent = e.target.parentNode
+          
+     parent.children[1].style.display = "none"
+      parent.children[2].style.display = "block"     
+     
+}})
 
 
 
